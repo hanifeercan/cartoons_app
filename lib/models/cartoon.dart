37b2b860c7cd90f0ab1a268ba001a950aba5ac0f
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-06-27 17:30:50 +0300

    Create cartoon

---
+++ lib/models/cartoon.dart
@@ -0,0 +1,16 @@
+{
+"title": "Star vs the Forces of Evil",
+"year": 1999,
+"creator": [
+"Stephen Hillenburg"
+],
+"rating": "TV-Y7",
+"genre": [
+"Action",
+"Adventure"
+],
+"runtime_in_minutes": 22,
+"episodes": 77,
+"image": "https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+"id": 3
+}
